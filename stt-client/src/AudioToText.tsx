1f32b1210d9c8f35961c777ee435b9c1ad9e1d4a
Hamza <untilhamza@gmail.com>
2022-12-17 20:54:01 +0900

    add reactboostrap and socket io on client

---
+++ stt-client/src/AudioToText.tsx
@@ -0,0 +1,177 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { default as React, useEffect, useState, useRef } from "react";
+import { Button } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
+import * as io from "socket.io-client";
+
+const sampleRate = 16000;
+
+const getMediaStream = () =>
+  navigator.mediaDevices.getUserMedia({
+    audio: {
+      deviceId: "default",
+      sampleRate: sampleRate,
+      sampleSize: 16,
+      channelCount: 1,
+    },
+    video: false,
+  });
+
+interface WordRecognized {
+  final: boolean;
+  text: string;
+}
+
+interface TranscribedText {
+  text: string;
+}
+
+interface AudioToTextPropTypes {
+  onDataChange: (data: TranscribedText) => void;
+  readOnly: boolean;
+  data: TranscribedText;
+}
+
+const AudioToText: React.FC<AudioToTextPropTypes> = ({}) => {
+  const [connection, setConnection] = useState<io.Socket>();
+  const [currentRecognition, setCurrentRecognition] = useState<string>();
+  const [recognitionHistory, setRecognitionHistory] = useState<string[]>([]);
+  const [isRecording, setIsRecording] = useState<boolean>(false);
+  const [recorder, setRecorder] = useState<any>();
+  const processorRef = useRef<any>();
+  const audioContextRef = useRef<any>();
+  const audioInputRef = useRef<any>();
+
+  const speechRecognized = (data: WordRecognized) => {
+    if (data.final) {
+      setCurrentRecognition("...");
+      setRecognitionHistory((old) => [data.text, ...old]);
+    } else setCurrentRecognition(data.text + "...");
+  };
+
+  const connect = () => {
+    connection?.disconnect();
+    const socket = io.connect("http://localhost:8081");
+    socket.on("connect", () => {
+      console.log("connected", socket.id);
+      setConnection(socket);
+    });
+
+    socket.emit("send_message", "hello world");
+
+    //TODO: put this back
+    socket.emit("startGoogleCloudStream");
+
+    socket.on("receive_message", (data) => {
+      console.log("received message", data);
+    });
+
+    socket.on("receive_audio_text", (data) => {
+      console.log("received audio text", data);
+      speechRecognized(data);
+      console.log("received audio text", data);
+    });
+
+    socket.on("disconnect", () => {
+      console.log("disconnected", socket.id);
+    });
+  };
+
+  const disconnect = () => {
+    if (!connection) return;
+    connection?.emit("endGoogleCloudStream");
+    connection?.disconnect();
+    processorRef.current?.disconnect();
+    audioInputRef.current?.disconnect();
+    audioContextRef.current?.close();
+    setConnection(undefined);
+    setRecorder(undefined);
+    setIsRecording(false);
+  };
+
+  useEffect(() => {
+    (async () => {
+      if (connection) {
+        if (isRecording) {
+          return;
+        }
+        //test connection
+        connection.emit("send_message", "testing audio connection");
+
+        const stream = await getMediaStream();
+
+        audioContextRef.current = new window.AudioContext();
+
+        await audioContextRef.current.audioWorklet.addModule(
+          "/src/worklets/recorderWorkletProcessor.js"
+        );
+
+        audioContextRef.current.resume();
+
+        audioInputRef.current =
+          audioContextRef.current.createMediaStreamSource(stream);
+
+        processorRef.current = new AudioWorkletNode(
+          audioContextRef.current,
+          "recorder.worklet"
+        );
+
+        processorRef.current.connect(audioContextRef.current.destination);
+        audioContextRef.current.resume();
+
+        audioInputRef.current.connect(processorRef.current);
+
+        processorRef.current.port.onmessage = (event: any) => {
+          const audioData = event.data;
+          console.log("process audio data", audioData);
+          connection.emit("send_audio_data", { audio: audioData });
+        };
+
+        setIsRecording(true);
+      } else {
+        console.log("no connection");
+      }
+    })();
+    return () => {
+      if (isRecording) {
+        processorRef.current?.disconnect();
+        audioInputRef.current?.disconnect();
+        if (audioContextRef.current?.state !== "closed") {
+          audioContextRef.current?.close();
+        }
+      }
+    };
+  }, [connection, isRecording, recorder]);
+
+  return (
+    <React.Fragment>
+      <p>{text || currentRecognition}</p>
+      <Container fluid className="py-5 bg-primary text-light text-center">
+        <Container>
+          <Button
+            className={isRecording ? "btn-danger" : "btn-outline-light"}
+            onClick={connect}
+            disabled={isRecording}
+          >
+            Start
+          </Button>
+          <Button
+            className="btn-outline-light"
+            onClick={disconnect}
+            disabled={!isRecording}
+          >
+            Stop
+          </Button>
+        </Container>
+      </Container>
+      <Container className="py-5 text-center">
+        {recognitionHistory.map((tx, idx) => (
+          <p key={idx}>{tx}</p>
+        ))}
+        <p>{currentRecognition}</p>
+      </Container>
+    </React.Fragment>
+  );
+};
+
+export default AudioToText;
