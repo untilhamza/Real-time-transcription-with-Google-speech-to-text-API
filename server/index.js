0a34ae22eedccb525dffc65527a7ab9839be3dc0
Hamza <untilhamza@gmail.com>
2022-12-17 20:31:15 +0900

    intial commit

---
+++ server/index.js
@@ -0,0 +1,141 @@
+const express = require("express");
+const speech = require("@google-cloud/speech");
+
+//use logger
+const logger = require("morgan");
+
+//use body parser
+const bodyParser = require("body-parser");
+
+//use corrs
+const cors = require("cors");
+
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+
+app.use(cors());
+app.use(logger("dev"));
+
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+const speechClient = new speech.SpeechClient();
+
+io.on("connection", (socket) => {
+  let recognizeStream = null;
+  console.log("** a user connected - " + socket.id + " **\n");
+
+  socket.on("disconnect", () => {
+    console.log("** user disconnected ** \n");
+  });
+
+  socket.on("send_message", (message) => {
+    console.log("message: " + message);
+    setTimeout(() => {
+      io.emit("receive_message", "got this message" + message);
+    }, 1000);
+  });
+
+  socket.on("startGoogleCloudStream", function (data) {
+    startRecognitionStream(this, data);
+  });
+
+  socket.on("endGoogleCloudStream", function () {
+    console.log("** ending google cloud stream **\n");
+    stopRecognitionStream();
+  });
+
+  socket.on("send_audio_data", async (audioData) => {
+    io.emit("receive_message", "Got audio data");
+    if (recognizeStream !== null) {
+      try {
+        recognizeStream.write(audioData.audio);
+      } catch (err) {
+        console.log("Error calling google api " + err);
+      }
+    } else {
+      console.log("RecognizeStream is null");
+    }
+  });
+
+  function startRecognitionStream(client) {
+    console.log("* StartRecognitionStream\n");
+    try {
+      recognizeStream = speechClient
+        .streamingRecognize(request)
+        .on("error", console.error)
+        .on("data", (data) => {
+          const result = data.results[0];
+          const isFinal = result.isFinal;
+
+          const transcription = data.results
+            .map((result) => result.alternatives[0].transcript)
+            .join("\n");
+
+          console.log(`Transcription: `, transcription);
+
+          client.emit("receive_audio_text", {
+            text: transcription,
+            final: isFinal,
+          });
+        });
+    } catch (err) {
+      console.error("Error streaming google api " + err);
+    }
+  }
+
+  function stopRecognitionStream() {
+    if (recognizeStream) {
+      console.log("* StopRecognitionStream \n");
+      recognizeStream.end();
+    }
+    recognizeStream = null;
+  }
+});
+
+server.listen(8081, () => {
+  console.log("WebSocket server listening on port 8081.");
+});
+
+// =========================== GOOGLE CLOUD SETTINGS ================================ //
+
+// The encoding of the audio file, e.g. 'LINEAR16'
+// The sample rate of the audio file in hertz, e.g. 16000
+// The BCP-47 language code to use, e.g. 'en-US'
+const encoding = "LINEAR16";
+const sampleRateHertz = 16000;
+const languageCode = "en-US"; //en-US
+const alternativeLanguageCodes = ["en-US", "ko-KR"];
+
+const request = {
+  config: {
+    encoding: encoding,
+    sampleRateHertz: sampleRateHertz,
+    languageCode: languageCode,
+    //alternativeLanguageCodes: alternativeLanguageCodes,
+    enableWordTimeOffsets: true,
+    enableAutomaticPunctuation: true,
+    enableWordConfidence: true,
+    enableSpeakerDiarization: true,
+    diarizationSpeakerCount: 2,
+    model: "video",
+    //model: "command_and_search",
+    useEnhanced: true,
+    speechContexts: [
+      {
+        phrases: ["hello", "안녕하세요"],
+      },
+    ],
+  },
+  interimResults: true,
+};
